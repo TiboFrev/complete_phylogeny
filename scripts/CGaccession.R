90d124340ca8bb3e603a19ad4bd8e37e326c9677
TiboFrev <135321460+TiboFrev@users.noreply.github.com>
2023-08-23 13:01:10 +0100

    Update CGaccession.R

--- scripts/CGaccession.R
+++ scripts/CGaccession.R
@@ -1,16 +1,26 @@
+##Import the necessary libraries
+
 library(rentrez)
 
+
+##This script takes in argument a string "species" which is the name of an organism
+##and a string "title" which is the name of the run (names of the files)
+##It returns a fasta file with every complete genomic sequences of the input organism available in Genbank
+
+##Arguments collection
 args <- commandArgs(trailingOnly = TRUE)
-species <- args
-sp <- paste (species, "[organism] AND complete genome [title]")
+species <- args[1] #ex : LSDV, Sheeppox_virus
+title <- args[2] #ex : 22_08_23
 
-query <- entrez_search(db="nuccore", term=sp ,retmax=99999) #extracting the IDs of every sequence that matches the query
-IDs <- query$ids #storing the IDs
+##Collecting the IDs of the sequences
+sp <- paste (species, "[organism] AND complete genome [title]") ##Query used to ask the database
+query <- entrez_search(db="nuccore", term=sp ,retmax=99999, use_history = TRUE) #extracting the IDs of every sequence that matches the query
+IDs <- query$ids  #storing the IDs
 
-
-sequences <- rentrez::entrez_fetch(db="nuccore", id=IDs, rettype="fasta") #With the IDs, extracting the fasta sequences
-titre = paste("full",species,"genomes.fasta",sep = "_")
-write(sequences, titre, sep="\n") #one fasta file is created, with every single sequence
+##Collecting the sequences
+sequences <- rentrez::entrez_fetch(db="nuccore", web_history=query$web_history, rettype="fasta") #With the IDs, extracting the fasta sequences
+titre = paste(title,"full_genomes.fasta",sep = "_") #Name of the fasta file
+write(sequences, titre, sep="\n",append=TRUE) #one fasta file is created, with every single sequence
 
 
 cat(IDs)
