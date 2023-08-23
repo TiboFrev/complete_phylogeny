c9b55a6528e6a07330e34fafaa185ff82302e157
TiboFrev <135321460+TiboFrev@users.noreply.github.com>
2023-08-23 13:14:32 +0100

    Update Treevisualisation.R

--- scripts/Treevisualisation.R
+++ scripts/Treevisualisation.R
@@ -1,31 +1,66 @@
+##Import the necessary libraries
 library(ape)
+library(phytools)
 
+##This script create 6 different picture files. 3 pdf and 3 png, which represent the phylogenetic trees of one sequence alignment
+##It takes two arguments:
+##  A tree file in the Newick format
+##  A string "title" which is the name of the run, applied to the title of the picture files
+
+##Argument collection
 args <- commandArgs(trailingOnly = TRUE)
 BestTree <- args[1]
-species <- args[2]
+title <- args[2]
 
+##Reading and rooting the tree
 tree <- read.tree(BestTree)
+tree<-midpoint.root(tree)
 
-png(file = paste(species,"phylogeny.png",sep = "_"), width = 1900, height = 900, units = "px")
 
-plot(tree, cex = 0.8, show.tip.label = TRUE, type = "phylogram", main = paste("Phylogeny of", species), no.margin = FALSE)
+############################## Creation of the different files #######################################
+
+
+##Phylogramm with labels
+#png file
+png(file = paste(title,"phylogram.png",sep = "_"), width = 1900, height = 900, units = "px")
+plot(tree, cex = 0.8, show.tip.label = TRUE, type = "phylogram", no.margin = FALSE)
 ape::add.scale.bar(cex = 0.8)
-dev.off()  
+dev.off() 
 
-pdf(file =paste(species,"phylogeny.pdf",sep = "_"), width = 100, height = 50)
 
-plot(tree, cex = 4,cex.main = 4, show.tip.label = TRUE, type = "phylogram", main = paste("Phylogeny of", species), no.margin = FALSE)
+#pdf file
+pdf(file =paste(title,"phylogram.pdf",sep = "_"), width = 100, height = 50)
+plot(tree, cex = 4,cex.main = 4, show.tip.label = TRUE, type = "phylogram", no.margin = FALSE)
 ape::add.scale.bar(cex=4)
 dev.off()
 
-png(file = paste(species,"phylogeny_nolabel.png",sep = "_"), width = 1900, height = 900, units = "px")
 
-plot(tree, cex = 0.8, show.tip.label = FALSE, type = "phylogram", main = paste("Phylogeny of", species), no.margin = FALSE)
+##Fan with labels
+#png file
+png(file = paste(title,"fan.png",sep = "_"), width = 1900, height = 900, units = "px")
+plot(tree, cex = 0.8, show.tip.label = TRUE, type = "fan", no.margin = FALSE)
+ape::add.scale.bar(cex = 0.8)
+dev.off()
+
+#pdf file
+pdf(file =paste(title,"fan.pdf",sep = "_"), width = 100, height = 50)
+plot(tree, cex = 4,cex.main = 4, show.tip.label = TRUE, type = "fan", no.margin = FALSE)
+ape::add.scale.bar(cex=4)
+dev.off()
+
+
+##Phylogramm without labels
+#png file
+png(file = paste(title,"phylogram_nolabel.png",sep = "_"), width = 1900, height = 900, units = "px")
+plot(tree, cex = 0.8, show.tip.label = FALSE, type = "phylogram", no.margin = FALSE)
 ape::add.scale.bar(cex = 0.8)
 dev.off()  
 
-pdf(file =paste(species,"phylogeny_nolabel.pdf",sep = "_"), width = 100, height = 50)
-
-plot(tree, cex = 4,cex.main = 4, show.tip.label = FALSE, type = "phylogram", main = paste("Phylogeny of", species), no.margin = FALSE)
+#pdf file
+pdf(file =paste(title,"phylogram_nolabel.pdf",sep = "_"), width = 100, height = 50)
+plot(tree, cex = 4,cex.main = 4, show.tip.label = FALSE, type = "phylogram", no.margin = FALSE)
 ape::add.scale.bar(cex=4)
 dev.off()
+
+
+######################################################################################################
