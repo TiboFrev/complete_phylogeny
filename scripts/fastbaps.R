c45e80145026d6fed60db21e9b6f453a88556f5a
TiboFrev <135321460+TiboFrev@users.noreply.github.com>
2023-08-23 13:21:01 +0100

    Update fastbaps.R

--- scripts/fastbaps.R
+++ scripts/fastbaps.R
@@ -1,3 +1,5 @@
+##Packages to install if necessary
+
 # update.packages(ask=F)
 knitr::opts_chunk$set(echo = T)
 # from https://github.com/gtonkinhill/fastbaps
@@ -9,6 +11,7 @@ knitr::opts_chunk$set(echo = T)
 #install.packages("devtools", ask=F)
 #install.packages("irlba", ask=F)
 
+##Import the necessary libraries
 library(irlba)
 library(ggtree)   # ggtree v2.4.1
 library(phytools) # phytools v0.7-70
@@ -21,21 +24,32 @@ library(RcppArmadillo)
 #devtools::install_github("gtonkinhill/fastbaps", force=T, type="source")
 library(fastbaps) # Load libraries
 
-samples = c()
+
+##This script uses the FastBAPS algorithm from Tonkin-Hill et al. For more information see github.com/gtonkinhill/fastbaps
+##It takes in argument:
+##  An alignment file in fasta format
+##  A tree file in Newick format
+##And it performs a Hierarchical Bayesian  clustering analysis
+
+##Arguments collection
 args <- commandArgs(trailingOnly = TRUE)
 fastaname = args[[1]]
 treename = args[[2]]
-   
+
+##Initialisation
+samples = c()   
 fastbapsplot = paste0("FastBAPS_", fastaname, ".png", sep="")
 heatmapplot = paste0("Heatmap_", fastaname, ".png", sep="")
-
 sparse.data <-import_fasta_sparse_nt(fastaname)
+
 
 # do clustering, get dk values, then Bayesian clustering
 sparse.data <- optimise_prior(sparse.data, type="optimise.symmetric")
-# hyperparameter: 0.044 for core
+
 best.partition <- best_baps_partition(sparse.data, fast_baps(sparse.data))
 # partition model
+
+##Plot the fastbaps clustering analysis
 plot.df <-data.frame(id=colnames(sparse.data$snp.matrix),
                         fastbaps=best.partition, stringsAsFactors=F)
 gg <- ggtree(read.tree(treename),linewidth=0.2) + geom_tiplab(hjust=0.06, size=0.8) 
@@ -44,10 +58,11 @@ png(fastbapsplot, height=980, width=980, units='px', res=300) # tree & groups
 print(facet_plot(gg, panel="fastbaps", data=plot.df, geom=geom_tile,
                     aes(x=fastbaps), color="blue")) 
 dev.off()
-   
+
+##Plot the heatmap of resampling analysis   
 dendro <- as.dendrogram(fast_baps(sparse.data)) # Heatmap
    
 png(heatmapplot, height=2000, width=2000, units='px', res=300) #  
 print(gplots::heatmap.2(boot_fast_baps(sparse.data), dendro, dendro,
                    tracecol=NA, margins=c(8,8), key=F, cex.lab=0.1))
-dev.off() # end function plotstuff
+dev.off() 
