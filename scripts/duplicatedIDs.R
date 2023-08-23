0b9392a78ee9653a081906de4ba8e5c60d56d187
TiboFrev <135321460+TiboFrev@users.noreply.github.com>
2023-08-23 13:01:29 +0100

    Update duplicatedIDs.R

--- scripts/duplicatedIDs.R
+++ scripts/duplicatedIDs.R
@@ -1,15 +1,23 @@
+
+##This script takes in argument the output text file of seqkit::rmdup, containing accession numbers
+##of duplicated sequences
+##It returns a text file containing the accession numbers of the removed sequences
+##with one accession number per lines
+
+
+##Arguments collection
 args <- commandArgs(trailingOnly = TRUE)
+dups <- readLines(args) 
 
-dups <- readLines(args)
-
-
+##Extracting the IDs in a new variable
 IDs <- c()
-for (i in 1:length(dups)){
-  for (j in 2:length(strsplit(dups[i], ",")[[1]])){
+for (i in 1:length(dups)){ #each line is one set of n duplicated sequences
+  for (j in 2:length(strsplit(dups[i], ",")[[1]])){ #The first ID is the kept sequence, all the others need to be stored
     IDs<-c(IDs,(strsplit(dups[i], ",")[[1]][j]))
   }
 }
 print(IDs)
 newIDs <- sapply(IDs, function(x) gsub(" ", "", x))
 
+##Creating the output file
 writeLines(newIDs, "IDstoremove.txt")
